caabf8f58a3c36ce0e51e2c5cfea2ff51ddc44c0
Xiang Mao <xiang.mao.2021@mumail.ie>
2021-04-17 22:40:17 +0100

    add jump tip page

--- src/main/webapp/static/js/jump/tip.js
+++ src/main/webapp/static/js/jump/tip.js
@@ -1,1 +1,25 @@
-1
+window.onload = function(){
+    let p_data = g_jsp_data;
+
+    pageJump(p_data.type, p_data.pares);
+}
+function pageJump(type, pares){
+    let addr = '';
+
+    switch (type){
+        case 'login':
+            addr += '../login/login.html';
+            break;
+        case 'register':
+            addr += '../register/register';
+            break;
+        default:
+            console.log('default');
+    }
+
+    addr += '?';
+    for (let i in pares)
+        addr += i + '=' + pares[i] + '&';
+
+    window.location.replace(addr);
+}
