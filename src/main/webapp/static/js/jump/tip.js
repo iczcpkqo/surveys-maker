d3898b2b01769d9d903a71f9a9632bd999c01aa0
Xiang Mao <xiang.mao.2021@mumail.ie>
2021-04-17 22:53:04 +0100

    update type in tip.js

--- src/main/webapp/static/js/jump/tip.js
+++ src/main/webapp/static/js/jump/tip.js
@@ -4,18 +4,18 @@ window.onload = function(){
     pageJump(p_data.type, p_data.pares);
 }
 function pageJump(type, pares){
-    let addr = '';
+    let addr = type;
 
-    switch (type){
-        case 'login':
-            addr += '../login/login.html';
-            break;
-        case 'register':
-            addr += '../register/register';
-            break;
-        default:
-            console.log('default');
-    }
+    // switch (type){
+    //     case 'login':
+    //         addr += '../login/login.html';
+    //         break;
+    //     case 'register':
+    //         addr += '../register/register';
+    //         break;
+    //     default:
+    //         console.log('default');
+    // }
 
     addr += '?';
     for (let i in pares)
