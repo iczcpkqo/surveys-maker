1525e1a7b68e74f735b0fcaeae94108372031f22
Xiang Mao <xiang.mao.2021@mumail.ie>
2021-04-18 19:20:31 +0100

    finish surveys-list.html

--- src/main/webapp/static/js/xiang_control/base.js
+++ src/main/webapp/static/js/xiang_control/base.js
@@ -46,3 +46,46 @@ function $(id){
 
     }
 }
+
+
+/**
+ *
+ * @param addr {string}, The http address of this link.
+ * @param pares {json}, pares = {
+ *                                  parameter 1: value of parameter 1,
+ *                                  parameter 2: value of parameter 2
+ *                                  ...
+ *                                  parameter n: value of parameter n
+ *                              }
+ * @returns link {string}, The whole address of this link.
+ */
+function linkMaker(addr, pares){
+    if(!pares || !addr)
+        return addr;
+
+    let link = addr + '?';
+    for(let i in pares)
+        link += i + '=' + pares[i] + '&'
+    link = link.substring(0, link.length - 1);
+
+    return link;
+}
+
+/**
+ * 复制内容到粘贴板
+ * content : 需要复制的内容
+ * message : 复制完后的提示，不传则默认提示"复制成功"
+ */
+function copyToClip(content, message) {
+    var aux = document.createElement("input");
+    aux.setAttribute("value", content);
+    document.body.appendChild(aux);
+    aux.select();
+    document.execCommand("copy");
+    document.body.removeChild(aux);
+    if (message == null) {
+        alert("The link has been copied to the clipboard.");
+    } else{
+        alert(message);
+    }
+}
