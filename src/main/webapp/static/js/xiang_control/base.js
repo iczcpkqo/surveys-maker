b43bb9b8d34e8cc8e7d90fe83a3a1256cc5a77ab
Xiang Mao <xiang.mao.2021@mumail.ie>
2021-04-19 23:00:37 +0100

    fix a bug about paging

--- src/main/webapp/static/js/xiang_control/base.js
+++ src/main/webapp/static/js/xiang_control/base.js
@@ -100,7 +100,8 @@ function addPaging(page, page_amount){
     let page_con = $('#page-num');
     let cls = '';
 
-    for (let i = page-num>0? page-num: 1; i<(page+num>page_amount? page_amount: page+num+1); i++) {
+    for (let i = page-num>0? page-num: 1; i<(page+num>page_amount? page_amount+1: page+num+1); i++) {
+        // alert(1);
         if (i === Number(page))
             cls = 'paging-li sel';
         else
