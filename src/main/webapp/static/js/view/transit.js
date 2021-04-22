2103cf0ae072fd85b9c8a4882ef3b6d5432b2dbe
Xiang Mao <xiang.mao.2021@mumail.ie>
2021-04-22 01:17:51 +0100

    change the link of jump of transit page and add client stat page

--- src/main/webapp/static/js/view/transit.js
+++ src/main/webapp/static/js/view/transit.js
@@ -1,14 +1,16 @@
 function iniPage(id){
+    let link = 'start';
+    let par = {
+        surveys_id: id
+    }
+
     $('#transit-tit').innerHTML = '点击开始答题';
     $('#transit-des').innerHTML = '点击后你将开始答题了';
     $('#transit-remark').innerHTML = '备注信息';
 
     $('#transit-operation').innerHTML = '<a id="transit-button" class="btn-1"></a>';
     $('#transit-button').addEventListener('click', ()=>{
-        let par = {
-            surveys_id: id
-        }
-        window.location.replace(linkMaker('client-view', par));
+        window.location.replace(linkMaker(link, par));
     })
 }
 
